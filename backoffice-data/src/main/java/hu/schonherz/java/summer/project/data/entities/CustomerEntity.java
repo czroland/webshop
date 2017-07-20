0198eefea4ea82d3dd21345b3ef9097cc0100f31
sutos88 <sutos88@gmail.com>
2017-07-20 16:31:33 +0200

    CustomerEntity added

---
+++ backoffice-data/src/main/java/hu/schonherz/java/summer/project/data/entities/CustomerEntity.java
@@ -0,0 +1,50 @@
+package hu.schonherz.java.summer.project.data.entities;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Entity
+@Table(name = "customers")
+public class CustomerEntity extends BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(40)")
+    private String fullName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(20)")
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String billingAddress;
+
+    @Column(nullable = false)
+    private String deliveryAddress;
+
+    public CustomerEntity(String username) {
+        this.username = username;
+    }
+
+}
