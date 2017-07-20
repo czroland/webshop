258c4a9a9d104e02f90d9decf39c801b42fa3fb0
DavidKiraly <hundood@gmail.com>
2017-07-20 23:30:52 +0200

    AddressEntity.java added

---
+++ backoffice-data/src/main/java/hu/schonherz/java/summer/project/data/entities/AddressEntity.java
@@ -0,0 +1,28 @@
+package hu.schonherz.java.summer.project.data.entities;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Entity
+@Table(name = "adresses")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AddressEntity extends BaseEntity{
+
+
+    private String zipCode;
+
+    private String city;
+
+    private String street;
+
+    private String number;
+
+}
