f8e37fb3fc53366d979dff395ffa16203afd10ed
DavidKiraly <hundood@gmail.com>
2017-07-20 15:54:35 +0200

    user entity added

--- backoffice-data/src/main/java/hu/schonherz/java/summer/project/data/entities/UserEntity.java
+++ backoffice-data/src/main/java/hu/schonherz/java/summer/project/data/entities/UserEntity.java
@@ -3,25 +3,30 @@ package hu.schonherz.java.summer.project.data.entities;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import java.io.Serializable;
 
-@Entity
-@Table(name = "user")
 @Data
-@EqualsAndHashCode(callSuper = false)
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class UserEntity extends BaseEntity implements Serializable {
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "users")
+public class UserEntity extends BaseEntity {
 
-    private static final long serialVersionUID = 1L;
 
+    @Column(nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String fullName;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String phone;
+
+    @Column(nullable = false)
     private String password;
 
 
