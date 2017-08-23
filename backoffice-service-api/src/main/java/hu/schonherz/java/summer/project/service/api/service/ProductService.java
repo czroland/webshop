9d285ac32d76315868ae12a6ac36cc729605b873
DavidKiraly <hundood@gmail.com>
2017-08-23 23:27:52 +0200

    Product Service added

---
+++ backoffice-service-api/src/main/java/hu/schonherz/java/summer/project/service/api/service/ProductService.java
@@ -0,0 +1,17 @@
+package hu.schonherz.java.summer.project.service.api.service;
+
+
+import hu.schonherz.java.summer.project.service.api.vo.ProductVo;
+
+import java.util.List;
+
+public interface ProductService {
+
+    void saveProduct(ProductVo productVo);
+
+    ProductVo getProductByName(String name);
+
+    ProductVo getProductById(long id);
+
+    ProductVo getProductByNumber(String number);
+}
