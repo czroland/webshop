2578ff907c6ae540aca550bcb5006efdc7a42b88
DavidKiraly <hundood@gmail.com>
2017-08-24 23:35:52 +0200

    ProductImageService added

---
+++ backoffice-service-api/src/main/java/hu/schonherz/java/summer/project/service/api/service/ProductImageService.java
@@ -0,0 +1,9 @@
+package hu.schonherz.java.summer.project.service.api.service;
+
+
+import hu.schonherz.java.summer.project.service.api.vo.ProductImageVo;
+
+public interface ProductImageService {
+
+    ProductImageVo getProductImageById(long id);
+}
