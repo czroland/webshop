35057fc6ab8267e8121c21d418a8eebf0e97052c
DavidKiraly <hundood@gmail.com>
2017-08-30 20:30:11 +0200

    Some fixes in Cart

--- backoffice-service/src/main/java/hu/schonherz/java/summer/project/service/impl/CartServiceImpl.java
+++ backoffice-service/src/main/java/hu/schonherz/java/summer/project/service/impl/CartServiceImpl.java
@@ -5,11 +5,15 @@ import hu.schonherz.java.summer.project.data.dao.CartDao;
 import hu.schonherz.java.summer.project.data.entities.CartEntity;
 import hu.schonherz.java.summer.project.service.api.service.CartService;
 import hu.schonherz.java.summer.project.service.api.vo.CartVo;
+import hu.schonherz.java.summer.project.service.api.vo.ProductVo;
 import hu.schonherz.java.summer.project.service.mapper.AbstractEntityVoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional(propagation = Propagation.REQUIRED)
@@ -30,7 +34,16 @@ public class CartServiceImpl extends AbstractEntityVoMapper implements CartServi
     }
 
     @Override
+    public List<CartVo> getAllCarts()
+    {
+       return map(cartRepository.findAll(), CartVo.class);
+    }
+    @Override
     public void removeCart(CartVo cartVo) {
+
+        List<ProductVo> emptyProductList = new ArrayList<>();
+        cartVo.setProductVos(emptyProductList);
+        cartRepository.save(map(cartVo,CartEntity.class));
 
     }
 }
